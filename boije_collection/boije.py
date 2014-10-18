a181a579e7543ecaa3cbeee01b5a9ae22da324c0
= <sohail288@hotmail.com>
2014-10-17 19:18:04 -0700

    program can now download from dictionary update dictionary and output to json

--- boije_collection/boije.py
+++ boije_collection/boije.py
@@ -1,5 +1,8 @@
-import os, sys
+# -*- coding: utf-8 -*-
+import os, sys, time, json
 import requests
+import unicodedata
+import string
 
 from bs4 import BeautifulSoup as BSoup
 
@@ -19,6 +22,32 @@ def getOrCreateComposerFolder(boije_folder, composer):
         os.makedirs(path_to_create)
     return path_to_create    
 
+def getScorePDF(score_url):
+    preappend_path = BOIJE_SITE_INDEX_URL[0:-12]
+    pdf_url = preappend_path + score_url
+    r = requests.get(pdf_url) 
+    return r
+
+def saveScorePDF(downloaded_score, score_name, composer_folder):
+    content = downloaded_score.content
+    file_path = os.path.join(composer_folder, "%s.pdf"%score_name)
+    with open(file_path, 'w') as f:
+        f.write(content)
+    return (True, file_path)
+
+def downloadAndSaveScore(boije_folder, composer, score, score_attributes):
+    composer_folder = getOrCreateComposerFolder(boije_folder, composer)
+    html = score_attributes[0]
+    try:
+        #json file indicates that this already exists
+        if score_attributes[2]:
+            raise Exception
+        r = getScorePDF(html)
+        saveScorePDF(r, score, composer_folder)
+        return True
+    except:
+        return False
+
 def getOrCreateBoijeFolder(destination_directory, boije_directory):
     
     path_to_create = os.path.join(destination_directory, boije_directory)
@@ -32,7 +61,60 @@ def getIndexSoup(boijelink):
     soup = BSoup(r.content)
     return soup
 
+def createJsonFile(json_file_name, boije_directory_name):
+    json_file_path = os.path.join(boije_directory_name, json_file_name)
+    if not os.path.exists(json_file_path):
+        create_json_file = open(json_file_path, 'w')
+        create_json_file.close()
+        return json_file_path
+    return 0
+
+
+def convertComposerName(composer_name):
+    composer_name = u'%s'%(composer_name)
+    composer_name = composer_name.translate(dict((ord(char), None) for char in ',.'))
+    composer_name = composer_name.split()
+    composer_name = '_'.join(composer_name)
+    if not composer_name.strip():
+        composer_name = 'anon'
+    return composer_name
+
+def convertScoreName(score_name):
+    score_name = unicode(score_name, 'utf-8')
+    score_name = ''.join([i for i in unicodedata.normalize('NFKD', score_name) if ord(i) < 128])
+    score_name = score_name.split('.')
+    score_name_to_return = ''
+    #hackish way of testing for multiple spaces
+    space_flag = False
+    for split_word in score_name:
+        for char in split_word:
+            if char.isalnum():
+                score_name_to_return += char
+                space_flag = False
+            elif char in string.punctuation:
+                score_name_to_return += '_'
+            elif char in ' ':
+                if not space_flag and score_name_to_return[-1] != '_':
+                    score_name_to_return += '_'
+                    #finds a space character, sets flag
+                    #next time around, it would have to visit the other elif loops
+                    space_flag = True
+        
+            else:
+                score_name_to_return += char
+        if score_name_to_return[-1] != '_':
+            score_name_to_return += '_'
+    #delete the last hyphen and return
+    while score_name_to_return[-1] == '_':
+        score_name_to_return = score_name_to_return[:-1]
+    #last check for an extra hyphen in the beginning
+    if score_name_to_return[0] == '_':
+        score_name_to_return = score_name_to_return[1:]
+    return score_name_to_return
+
+
 def convertIndexToDictionary(soup):
+    #final dictionary will be {composer:{score1:(html, boije_number, downloaded) 
     dictionary_of_composers_and_their_pieces = {}
     table = soup.table
     rows = table.find_all('tr')
@@ -41,21 +123,41 @@ def convertIndexToDictionary(soup):
         for column in row.find_all('td'):
             list_of_row_components.append(column.contents[0])
         #composer is the first element, followed by piece/html of file, last is boije_number
-        composer = list_of_row_components[0]
-        #manipulate composer by stripping off all parts
-        #first check to see if composer field is blank
-        composer = composer.translate(dict((ord(char), None) for char in ',.'))
-        composer = composer.split()
-        composer = '_'.join(composer)
-        if not composer.strip():
-            composer = 'anon'
-        #next we have this composers scores
-        if dictionary_of_composers_and_their_pieces.get(composer):
-            dictionary_of_composers_and_their_pieces[composer] += list_of_row_components[1]
-        else:
-            dictionary_of_composers_and_their_pieces[composer] = []
-            dictionary_of_composers_and_their_pieces[composer] += list_of_row_components[1]
+        #BeautifulSoup text is unicode, my other function can't decode unicode, so let's encode it here.
+        composer = convertComposerName(list_of_row_components[0])
+        score = convertScoreName(list_of_row_components[1].text.encode('utf-8'))
+        score_html = list_of_row_components[1].get('href')
+        #boije number appears "Boije X", so all you have to do is split it and get the last element
+        boije_number = list_of_row_components[2].split()[-1]
+        #I'm assuming this function will run to initialize the data, so nothing should be downloaded
+        downloaded = False
+        #sometimes a composers name is mispelled.  I don't have time to figure out how to remove duplicates
+        #that is why, the following code checks to see if a composer already has his dictionary of scores
+        #if he doesn't, a new dictionary will be associated with the composer.
+        if not dictionary_of_composers_and_their_pieces.get(composer):
+            dictionary_of_composers_and_their_pieces[composer] = {}
+        score_dict = {score: [score_html, boije_number, downloaded]}
+        dictionary_of_composers_and_their_pieces[composer].update(score_dict)
     return dictionary_of_composers_and_their_pieces
+
+def convertIndexToJson(index_dictionary, json_file_path):
+    if not os.path.exists(json_file_path) or os.path.getsize(json_file_path) > 0:
+        return 0
+    with open(json_file_path, 'w') as fp:
+        json.dump(index_dictionary, fp, sort_keys = True, indent = 4)
+    return 1 
+
+def convertJsonToDict(json_file_path):
+    if not os.path.exists(json_file_path):
+        return 0
+    with open(json_file_path, 'r') as fp:
+        index_dictionary = json.load(fp)
+    return index_dictionary
+
+def updateJsonFile(index_dictionary, json_file_path):
+    with open(json_file_path, 'w') as fp:
+        json.dump(index_dictionary, fp, sort_keys = True, indent = 4)
+    return 1
 
 def main():
     boije_folder = getOrCreateBoijeFolder(DESTINATION_DIRECTORY, BOIJE_DIRECTORY_NAME)
--- boije_collection/tests.py
+++ boije_collection/tests.py
@@ -1,4 +1,5 @@
-import os, sys, shutil
+# -*- coding: utf-8 -*-
+import os, sys, shutil, time, copy
 import  unittest
 
 import requests
@@ -6,8 +7,20 @@ from bs4 import BeautifulSoup as BSoup
 
 from boije import BOIJE_SITE_INDEX_URL, DESTINATION_DIRECTORY,\
 BOIJE_DIRECTORY_NAME, getOrCreateBoijeFolder, getOrCreateComposerFolder,\
-boijeLink, getIndexSoup, convertIndexToDictionary
+boijeLink, getIndexSoup, convertIndexToDictionary, convertComposerName,\
+convertScoreName, getScorePDF, saveScorePDF, createJsonFile, convertIndexToJson,\
+convertJsonToDict, downloadAndSaveScore, updateJsonFile
 
+class DirectorySetupAndRemovalMixin(object):
+    #To be used only after all of these functions have been tested
+    def setUp(self):
+        self.directory_path = getOrCreateBoijeFolder(DESTINATION_DIRECTORY, BOIJE_DIRECTORY_NAME)
+          
+        
+    def tearDown(self):
+        for root, dirs, files in os.walk(self.directory_path):
+            if BOIJE_DIRECTORY_NAME in root:
+                shutil.rmtree(root)
 
 class BoijeSiteRetrievalTests(unittest.TestCase):
 
@@ -30,9 +43,73 @@ class BoijeSiteRetrievalTests(unittest.TestCase):
         self.assertEqual(status_code, 200)
 
 
+class ScoreRetrieveAndStoreTests(DirectorySetupAndRemovalMixin, unittest.TestCase):
+
+    def setUp(self):
+        super(ScoreRetrieveAndStoreTests, self).setUp()
+        self.score_name = 'Op_22_Trois_Sonates'
+        self.composer = 'Carcassi_M'
+        self.html = "pdf/Boije%2074.pdf"
+
+    def testRetrieveScore(self):
+        full_url = 'http://biblioteket.statensmusikverk.se/ebibliotek/boije/%s'%self.html
+
+        downloaded_score = getScorePDF(self.html)
+        score_pdf = downloaded_score.content
+        status_code = downloaded_score.status_code
+
+        #All pdfs start with PDF in the beginning
+        self.assertEqual(full_url, downloaded_score.url)
+        self.assertEqual(200, status_code)
+        self.assertIn('%PDF-', score_pdf[0:5])
+
+    def testStoreScore(self):
+        downloaded_score = getScorePDF(self.html)
+        create_boije_folder = getOrCreateBoijeFolder(DESTINATION_DIRECTORY, BOIJE_DIRECTORY_NAME)
+        create_composer_folder = getOrCreateComposerFolder(create_boije_folder, self.composer)
+        downloaded_score = getScorePDF(self.html)
+        path_to_score_should_be = os.path.join(create_composer_folder, '%s.pdf'%self.score_name)
+
+        #The output of saveScorePDF will be a tuple (SUCCESS_CODE, PATH_TO_DOWNLOADED_SCORE)
+        saving_score_to_folder = saveScorePDF(downloaded_score, self.score_name, create_composer_folder)
+        truth_of_score, path_of_score = saving_score_to_folder
+
+        self.assertTrue(truth_of_score)
+        self.assertEqual(path_of_score, path_to_score_should_be)
+
+
+
 class BoijeLetterIndexTests(unittest.TestCase):
 
+    def testMakeComposerNameComputerReady(self):
+        composer_1 = "Castillo, D. del"
+        composer_2 = "     ."
+        
+        converted_composer_1 = convertComposerName(composer_1)
+        converted_composer_2 = convertComposerName(composer_2)
 
+        self.assertEqual('Castillo_D_del', converted_composer_1)
+        self.assertEqual('anon', converted_composer_2)
+
+    def testMakeScoreNameComputerReady(self):
+        score_1 = 'Op. 24. Andante. Se: Gitarristische Vereinigung ... XVI. Jahrg. Nr 3. 1915'
+        score_2 = 'Op. 22. Trois Sonates ...'
+        score_3 = 'Oeuvres pour Guitare. No 1. Ètude.'
+        score_4 = '”ERATO” Auswahl beliebter Gesänge ... No. 18. Santa Lucia! ...' 
+        test_string  = '"órgão"'
+
+        converted_score_1 = convertScoreName(score_1)
+        converted_score_2 = convertScoreName(score_2)
+        converted_score_3 = convertScoreName(score_3)
+        converted_score_4 = convertScoreName(score_4)
+        converted_score_5 = convertScoreName(test_string)
+
+        self.assertEqual('Op_24_Andante_Se_Gitarristische_Vereinigung_XVI_Jahrg_Nr_3_1915', converted_score_1)
+        self.assertEqual('Op_22_Trois_Sonates', converted_score_2)
+        self.assertEqual('Oeuvres_pour_Guitare_No_1_Etude', converted_score_3)
+        self.assertEqual('ERATO_Auswahl_beliebter_Gesange_No_18_Santa_Lucia', converted_score_4)    
+        self.assertEqual('orgao', converted_score_5)
+    
     def testRetrieveLetterIndexSoup(self):
         index_to_check = 'c'
 
@@ -42,17 +119,34 @@ class BoijeLetterIndexTests(unittest.TestCase):
 
     def testConvertRowEntiresToDictionary(self):
         #checks to see if an index can be made into a dict
-        #where dict = {'artist':[score1, score2, score3, score4]}
+        #where dict = {'composer': {score1: (html, boije_number, downloaded)}, {score2: ...}}
         index_to_check = 'c'
         link_to_check = boijeLink(index_to_check)
         soup = getIndexSoup(link_to_check)
 
         dictionary_of_values = convertIndexToDictionary(soup)
+        carcassi = dictionary_of_values.get('Carcassi_M')
 
+        #remember that, the return of convertIndexToDictionary returns a dictioniary of composers
+        #the dictionary will look like
+        # {'composer': {'score1': ['html', 'boije', Downloaded], 'score2': [...]}, 'composer2': {...}...}
         self.assertEqual(len(dictionary_of_values['Calegari_F']), 2)
         self.assertIn('Carcassi_M', dictionary_of_values)
         self.assertIn('anon', dictionary_of_values)
-        self.assertIn('Coste_N', dictionary_of_values) 		
+        self.assertIn('Coste_N', dictionary_of_values)      
+        #now check to see if some scores are found
+        self.assertIn('Op_1_3_Sonates', dictionary_of_values['Carcassi_M'])
+        self.assertIn('Op_13_4_Potpourris_des_plus_jolis_Airs_de_operas_de_Rossini', dictionary_of_values['Carcassi_M'])
+        #now check to see if two of the above match the actual values from boije
+        self.assertNotEqual(0, carcassi)
+        self.assertEqual("pdf/Boije%2074.pdf", carcassi.get('Op_1_3_Sonates')[0])
+        self.assertEqual("74", carcassi.get('Op_1_3_Sonates')[1])
+        self.assertEqual(False, carcassi.get('Op_1_3_Sonates')[2])
+        
+    
+
+#Okay I need a separate test to test whether a json file exists already
+
 
 class DirectoryTests(unittest.TestCase):
 
@@ -78,16 +172,141 @@ class DirectoryTests(unittest.TestCase):
         self.assertTrue(created_path_exists)
         self.assertEqual(self.path, self.created_path)
 
+
     def tearDown(self):
         for root, dirs, files in os.walk(self.path):
             if BOIJE_DIRECTORY_NAME in root:
                 shutil.rmtree(root)
 
 
+class CreateReadIndexTests(DirectorySetupAndRemovalMixin, unittest.TestCase):
 
+
+    def testCreateIndexFile(self):
+        json_file_name = 'boije_collection_test.json'
+        create_boije_directory = getOrCreateBoijeFolder(DESTINATION_DIRECTORY, BOIJE_DIRECTORY_NAME)
+        #this test will check to see if a function exists to create a json index file
+        file_path = os.path.join(self.directory_path, json_file_name)
+        #file returns path or 0
+        json_file_created = createJsonFile(json_file_name, create_boije_directory)
+
+        self.assertEqual(file_path, json_file_created)
+        
+
+    def testJsonFileExists_soCreateShouldFail(self):
+        json_file_name = 'boije_collection_test.json'
+        create_boije_directory = getOrCreateBoijeFolder(DESTINATION_DIRECTORY, BOIJE_DIRECTORY_NAME)
+        create_json_file = createJsonFile(json_file_name, create_boije_directory)
+
+        create_json_file_again = createJsonFile(json_file_name, create_boije_directory)
+
+        self.assertEqual(0, create_json_file_again) 
+
+class JSONFileTests(DirectorySetupAndRemovalMixin, unittest.TestCase):
+
+    def testSaveIndexToJSONAndReadIt(self):
+       #checks to see if an index that is converted to a dict can be written into a file with json format
+        index_to_check = 'c'
+        create_boije_directory = getOrCreateBoijeFolder(DESTINATION_DIRECTORY, BOIJE_DIRECTORY_NAME)
+        json_file_name = 'boije_collection_test.json'
+        json_file_path = createJsonFile(json_file_name, create_boije_directory)
+        link_to_check = boijeLink(index_to_check)
+        soup = getIndexSoup(link_to_check)
+        dictionary_of_values = convertIndexToDictionary(soup)
+        #Our function will use a try/except block, that will will return a 1 or a 0 depending on success/failure
+        dictionary_to_json = convertIndexToJson(dictionary_of_values, json_file_path)
+        
+        self.assertEqual(1, dictionary_to_json)
+        json_file_size = os.path.getsize(json_file_path)
+        json_file_exists = os.path.exists(json_file_path)
+        self.assertTrue(json_file_exists)
+        self.assertGreater(json_file_size, 0)
+        
+        
+        #now we have a file that has something written to it.
+        #We must  now check to see if when we read it, it will equate to dictionary_of_values
+
+        convert_json_to_dict = convertJsonToDict(json_file_path)
+        self.assertEqual(convert_json_to_dict, dictionary_of_values)
+
+
+    def testReadJsonFile_modifyAnd_SaveIt(self):
+        index_to_check = 'c'
+        create_boije_directory = getOrCreateBoijeFolder(DESTINATION_DIRECTORY, BOIJE_DIRECTORY_NAME)
+        composer = 'Carcassi_M'
+        score = 'Op_1_3_Sonates'
+        json_file_name = 'boije_collection_test.json'
+        json_file_path = createJsonFile(json_file_name, create_boije_directory)
+        link_to_check = boijeLink(index_to_check)
+        soup = getIndexSoup(link_to_check)
+        dictionary_of_values = convertIndexToDictionary(soup)
+        dictionary_to_json = convertIndexToJson(dictionary_of_values, json_file_path)
+        convert_json_to_dict = convertJsonToDict(json_file_path)
+
+        score_dictionary = copy.deepcopy(convert_json_to_dict)
+        #make a function that wraps a try/except block
+        #this function will need to copy the dictionary, and depending on whether 
+        #a download succeeds.  It will update the 'downloaded' field from the dict
+        #it will then return the dict
+        carcassi = score_dictionary.get(composer)
+        score_attributes = carcassi.get(score)
+        html = score_attributes[0]
+        
+        self.assertFalse(score_attributes[2])
+
+        #it will be better if this updating function takes in score attributes
+
+        downloaded_file = downloadAndSaveScore(create_boije_directory, composer, score, score_attributes)
+        
+        self.assertTrue(downloaded_file)
+        #set score_dictionary value to True
+        score_dictionary[composer][score][2] = downloaded_file
+        self.assertTrue(score_dictionary.get(composer).get(score)[0])
+        
+        self.assertNotEqual(score_dictionary[composer][score][2], convert_json_to_dict[composer][score][2])
+
+        convert_dict_to_json = updateJsonFile(score_dictionary, json_file_path)
+        convert_json_to_dict_again = convertJsonToDict(json_file_path)
+
+        self.assertEqual(convert_json_to_dict_again, score_dictionary)
+        
+
+    def testDownloadScore_tryDownloadAgain_ResultsInError(self):
+                        
+        index_to_check = 'c'
+        create_boije_directory = getOrCreateBoijeFolder(DESTINATION_DIRECTORY, BOIJE_DIRECTORY_NAME)
+        composer = 'Carcassi_M'
+        score = 'Op_1_3_Sonates'
+        json_file_name = 'boije_collection_test.json'
+        json_file_path = createJsonFile(json_file_name, create_boije_directory)
+        link_to_check = boijeLink(index_to_check)
+        soup = getIndexSoup(link_to_check)
+        dictionary_of_values = convertIndexToDictionary(soup)
+        dictionary_to_json = convertIndexToJson(dictionary_of_values, json_file_path)
+        convert_json_to_dict = convertJsonToDict(json_file_path)
+        score_dictionary = copy.deepcopy(convert_json_to_dict) 
+        score_attributes = convert_json_to_dict = score_dictionary.get(composer).get(score)
+
+        download_file = downloadAndSaveScore(create_boije_directory, composer, score, score_attributes)
+
+        self.assertTrue(download_file)
+        #now to save json file and reload it.
+        score_dictionary[composer][score][2] = download_file
+        convert_dict_to_json = updateJsonFile(score_dictionary, json_file_path)
+        convert_json_to_dict_again = convertJsonToDict(json_file_path)
+
+        score_dictionary = copy.deepcopy(convert_json_to_dict_again)
+        score_attributes = score_dictionary.get(composer).get(score)
+
+
+        download_file = downloadAndSaveScore(create_boije_directory, composer, score,  score_attributes)
+
+        self.assertFalse(download_file)
 
 if __name__ == '__main__':
-    test_classes_to_run = [BoijeSiteRetrievalTests, DirectoryTests,BoijeLetterIndexTests ]
+    test_classes_to_run = [BoijeSiteRetrievalTests, DirectoryTests, BoijeLetterIndexTests,
+                            ScoreRetrieveAndStoreTests, 
+                            CreateReadIndexTests, JSONFileTests]
     loader = unittest.TestLoader()
 
     suites_list = []
